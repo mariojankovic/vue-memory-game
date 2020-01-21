8e97adc5e7431a22c6e23cff0624d7fea54c44bd
Mario Jankovic <majankovi@gmail.com>
2020-01-21 16:56:57 +0100

    Refines logic, adds additinal styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
-    <p>clicks: {{ clicks }}</p>
-    <p>parovi: {{ pairs }}</p>
-    <p>prvi: {{ first }}</p>
-    <p>drugi: {{ second }}</p>
-
-    <ul class="cards">
-      <Card v-for="n in 12" :key="n" :n="n" @click.native="checkCard(n)"/>
+    <ul class="stats">
+      <li>No. of clicks: {{ clicks }}</li>
+      <li>Matched pairs: {{ pairs }}</li>
+      <li>First click: {{ first }}</li>
+      <li>Second click: {{ second }}</li>
     </ul>
+
+    <p>
+      <button @click="resetGame">New game</button>
+    </p>
+
+    <ul class="cards" :class="[{ 'is-Waiting': !clickable }, { 'is-Inactive': win }]" :key="game">
+      <Card
+        v-for="n in 12"
+        :key="n"
+        :n="n"
+        @click.native="checkCard(n)"
+        :class="[{ 'is-Clicked': clicked.includes(n) }, { 'is-Revealed': pairs.includes(n) || pairs.includes(n-1) }]"
+      />
+    </ul>
+
+    <transition name="fade">
+      <div class="won" v-if="win">
+        <h2>Congratulations!</h2>
+        <p>Game finished in {{ clicks }} clicks.</p>
+        <button @click="resetGame">Nova igra</button>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -24,25 +44,61 @@ export default {
       clicks: 0,
       pairs: [],
       first: null,
-      second: null
+      second: null,
+      clickable: true,
+      clicked: [],
+      game: 0
     };
   },
   methods: {
     checkCard(n) {
-      this.clicks++;
+      this.clicks++; // increment clicks each time we reveal a card
+      this.clicked.push(n); // check which cards are being clicked in current session
 
       if (n % 2 === 0) {
+        // decrease index number for even cards to create pairs
         n = n - 1;
       }
 
       if (this.clicks % 2 === 0) {
-        this.second = n;
+        // check for every second click
+        this.second = n; // define second clicked card in our current session
 
-        if (this.first === this.second) this.pairs.push(this.first);
+        if (this.first === this.second) {
+          // if pairs match, add them to our array called pairs
+          this.pairs.push(this.first);
+        } else {
+          // if pairs don't match reset stuff
+          this.clickable = false; // disable clicks if two pairs are being shown that don't match
+
+          setTimeout(() => {
+            // set a timer to hide both cards that didn't match in our current session
+            this.clickable = true;
+            this.second = null;
+            this.clicked = [];
+          }, 1000);
+        }
       } else {
+        // this basically starts a new "click" session, meaning that we specify our first card and remove the second card from our previous session
         this.first = n;
         this.second = null;
       }
+    },
+    resetGame() {
+      // self-explanatory. Resets all rules
+      this.pairs = [];
+      this.clicks = 0;
+      this.first = this.second = null;
+      this.clickable = true;
+      this.clicked = [];
+      this.game++; // increment the :key value to re-order all cards for a new game
+    }
+  },
+  computed: {
+    win() {
+      // if all pairs have been revealed set winning conditions to true
+      if (this.pairs.length === 6) return true;
+      return false;
     }
   }
 };
@@ -56,22 +112,64 @@ img {
 
 .cards {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: repeat(3, minmax(200px, 1fr));
   grid-gap: 20px;
-  margin: 0;
   padding: 0;
+  max-width: 700px;
+  margin: 0 auto;
+  transition: 300ms;
 }
 
-.card {
+.cards.is-Waiting {
+  pointer-events: none;
+}
+
+.cards.is-Inactive {
+  opacity: 0.5;
+}
+
+.stats {
+  background: rgba(0, 0, 0, 0.9);
+  padding: 20px;
+  margin: 0;
+  padding: 10px;
+  color: white;
+  font-size: 12px;
+  position: fixed;
+  top: 0;
+  right: 0;
+}
+
+.stats li {
   display: block;
+  text-align: left;
+}
+
+.won {
+  position: fixed;
+  padding: 30px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  border-radius: 5px;
+  z-index: 100;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.5);
+  min-width: 300px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 500ms;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 0;
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,64 @@
 <template>
   <li class="card" :style="{ order: Math.floor(Math.random() * 100) + 1 }">
-    <img :src="`https://picsum.photos/id/${cardID * 10}/200/200`" alt>
+    <img :src="`https://picsum.photos/id/${cardID * 10}/220/220`" alt>
   </li>
 </template>
+
+<style>
+.card {
+  display: block;
+  background-color: #DFDBE5;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='88' height='88' viewBox='0 0 88 88'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath fill-rule='evenodd' d='M29.42 29.41c.36-.36.58-.85.58-1.4V0h-4v26H0v4h28c.55 0 1.05-.22 1.41-.58h.01zm0 29.18c.36.36.58.86.58 1.4V88h-4V62H0v-4h28c.56 0 1.05.22 1.41.58zm29.16 0c-.36.36-.58.85-.58 1.4V88h4V62h26v-4H60c-.55 0-1.05.22-1.41.58h-.01zM62 26V0h-4v28c0 .55.22 1.05.58 1.41.37.37.86.59 1.41.59H88v-4H62zM18 36c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H20a2 2 0 0 1-2-2zm0 16c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H20a2 2 0 0 1-2-2zm16-26a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zM34 58a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v4a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-4zM34 78a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-6zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-6zM34 4a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2V4zm16 0a2 2 0 0 1 2-2 2 2 0 0 1 2 2v6a2 2 0 0 1-2 2 2 2 0 0 1-2-2V4zm-8 82a2 2 0 1 1 4 0v2h-4v-2zm0-68a2 2 0 1 1 4 0v10a2 2 0 1 1-4 0V18zM66 4a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0V4zm0 72a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0v-8zm-48 0a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0v-8zm0-72a2 2 0 1 1 4 0v8a2 2 0 1 1-4 0V4zm24-4h4v2a2 2 0 1 1-4 0V0zm0 60a2 2 0 1 1 4 0v10a2 2 0 1 1-4 0V60zm14-24c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H58a2 2 0 0 1-2-2zm0 16c0-1.1.9-2 2-2h10a2 2 0 1 1 0 4H58a2 2 0 0 1-2-2zm-28-6a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm8 26a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM36 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm-8-8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 68a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16-34a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm16-12a2 2 0 1 0 0 4 6 6 0 1 1 0 12 2 2 0 1 0 0 4 10 10 0 1 0 0-20zm-64 0a2 2 0 1 1 0 4 6 6 0 1 0 0 12 2 2 0 1 1 0 4 10 10 0 1 1 0-20zm56-12a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 48a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm-48 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0-48a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm24 32a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-4a6 6 0 1 0 0-12 6 6 0 0 0 0 12zm36-36a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM10 44c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zm56 0c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zm8 24c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zM3 68c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4H5a2 2 0 0 1-2-2zm0-48c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4H5a2 2 0 0 1-2-2zm71 0c0-1.1.9-2 2-2h8a2 2 0 1 1 0 4h-8a2 2 0 0 1-2-2zm6 66a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM8 86a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0-68A6 6 0 1 1 8 2a6 6 0 0 1 0 12zm0-4a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm36 36a2 2 0 1 0 0-4 2 2 0 0 0 0 4z'/%3E%3C/g%3E%3C/svg%3E");
+  border-radius: 5px;
+  border: 1px solid #CBC5D4;
+  transition: 300ms;
+  cursor: pointer;
+  overflow: hidden;
+}
+
+.card:hover {
+  background-color: rgb(193, 185, 206);
+}
+
+.card img {
+  transition: 300ms;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0;
+}
+
+.card.is-Clicked {
+  pointer-events: none;
+  transform: scale(1.1) rotate(3deg);
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+}
+
+.card.is-Clicked:nth-child(odd) {
+  transform: scale(1.1) rotate(-3deg);
+}
+
+.card.is-Clicked img {
+  opacity: 1;
+}
+
+.card.is-Revealed {
+  transform: none !important;
+  box-shadow: none;
+  pointer-events: none;
+}
+
+.card.is-Revealed img {
+  opacity: 1;
+}
+</style>
+
 
 <script>
 export default {
   props: ["n"],
   computed: {
     cardID() {
-      // return this.n % 2 === 0 ? this.n - 1 : this.n;
-
       if (this.n % 2 === 0) {
         return this.n - 1;
       } else {
